4da32253f75eed95f458c75606b5e5f16e401fb1
curiouscoder13 <172529323+curiouscoder13@users.noreply.github.com>
2024-08-27 22:17:14 +0800

    added functionality in giftRoute, db and import data to mongodb

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,6 +1,6 @@
 // db.js
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
@@ -9,20 +9,27 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+  if (dbInstance) {
+    return dbInstance;
+  }
 
-    const client = new MongoClient(url);      
+  const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+  // Task 1: Connect to MongoDB
+  // {{insert code}}
+  await client.connect();
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+  // Task 2: Connect to database giftDB and store in variable dbInstance
+  //{{insert code}}
+  dbInstance = await client.db(dbName);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+  // Task 4: Close MongoDB connection
+  // {{insert code}}
+  await client.close();
+
+  // Task 3: Return database instance
+  // {{insert code}}
+  return dbInstance;
 }
 
 module.exports = connectToDatabase;
